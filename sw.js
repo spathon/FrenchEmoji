fd93f50c360ad5a2b6a0a9f6b3d216d9b154659a
spathon <patrik@spathon.com>
2026-03-11 19:42:56 +0100

    Css to own file

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'fr-moji-v2';
-const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
+const ASSETS = ['./', './index.html', './style.css', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
